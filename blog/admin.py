4f16c6d22c29bc9b88a95c9d4344e17a5dfb99c7
Seyed Mohammad Ahmadi <106057277+Mamad66Ahmadi@users.noreply.github.com>
2026-02-02 19:39:21 +0330

    added meta for database
    in this part i have imported some example posts to the dataset and set the settings of my admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+
+#@admin.register(Post)  (optional, instead of: admin.site.register)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('id','title','counted_views','status','published_date','created_date')
+    list_filter = ('status',)
+    #ordering = ['created_date']
+    search_fields = ['title','content']
+
+    class Meta:
+        ordering = ['created_date']
+admin.site.register(Post,PostAdmin)
