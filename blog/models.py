4aef17084081eda55be25cb205b30079e1ea857f
Seyed Mohammad Ahmadi <106057277+Mamad66Ahmadi@users.noreply.github.com>
2026-02-03 17:00:06 +0330

    added category as many to many field

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
+    category = models.ManyToManyField(Category)
 
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
@@ -19,3 +28,4 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.id}"
+    
