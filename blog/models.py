d1516f267b9570191eb266737a566255a0bdb91c
Seyed Mohammad Ahmadi <106057277+Mamad66Ahmadi@users.noreply.github.com>
2026-02-03 14:26:07 +0330

    added image field
    in this section the images are shown in a dynamic amnner

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
+    image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
 
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
