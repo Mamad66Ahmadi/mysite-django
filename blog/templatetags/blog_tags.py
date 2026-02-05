77a80e890ffdbf711f31fb114d29adde7a84df83
Seyed Mohammad Ahmadi <106057277+Mamad66Ahmadi@users.noreply.github.com>
2026-02-05 15:58:13 +0330

    added categories as inclusion tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post
+from blog.models import Category
 
 
 register = template.Library()
@@ -9,3 +10,17 @@ register = template.Library()
 def latestposts(arg=3):
     posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts':posts}
+
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status =1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for category in categories:
+        cat_dict[category] = posts.filter(category=category).count()
+    
+    # Sort by count in descending order
+    sorted_categories = sorted(cat_dict.items(), key=lambda x: x[1], reverse=True)
+    
+    return {'categories': sorted_categories}
